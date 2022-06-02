66d169195e57bd14f5aadf1df5eb40fdf6f562a4
Andres Castillo <pettrovich@gmail.com>
2022-06-02 02:50:40 -0500

    controllers boilerplate

---
+++ api/src/controllers/getTemperaments.js
@@ -0,0 +1,5 @@
+
+
+async function getAllTemperaments(res) {}
+
+module.exports={getAllTemperaments};
---
+++ api/src/controllers/postDog.js
@@ -0,0 +1,5 @@
+
+
+async function postDog (name,height,weight,lifespan,temperaments,res) {}
+
+module.exports={postDog};
