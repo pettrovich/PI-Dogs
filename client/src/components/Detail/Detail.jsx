70092fbcf067073494fb5671373b91c52c2f2154
Andres Castillo <pettrovich@gmail.com>
2022-06-08 11:07:47 -0500

    detail view

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -4,6 +4,7 @@ import {useParams} from "react-router-dom";
 
 import {getDogById} from '../actions';
 import NavBar from "../NavBar/NavBar";
+import './Detail.css';
 
 export default function Detail() {
     const dispatch = useDispatch();
@@ -15,16 +16,22 @@ export default function Detail() {
     return (
         <div>
             <NavBar page={'detail'} />
-            {dog.length > 0 ?<div>
-            <h1>{dog[0].name}</h1>
-            {dog[0].image ? <img src={dog[0].image} alt={dog[0].name} width="400px" /> : ''}
-            <h5>Weight: {dog[0].weight}</h5>
-            <h5>Height: {dog[0].height}</h5>
-            <h5>Lifespan: {dog[0].lifespan}</h5>
-            <h5>Temperaments:</h5>
-            <ul>
-                {dog[0].temperaments.map(temperament => <li key={temperament}>{temperament}</li>)}
-            </ul>
-        </div> : <div className="loader"></div>}</div>
+            {dog.length > 0 ?
+            <div className='details'>
+                {dog[0].image ? <img src={dog[0].image} alt={dog[0].name}/> : ''}
+                <div className='box'>
+                    <h1>{dog[0].name}</h1>
+                    <h5>Weight: {dog[0].weight}</h5>
+                    <h5>Height: {dog[0].height}</h5>
+                    <h5>Lifespan: {dog[0].lifespan}</h5>
+                    <h5>Temperaments:</h5>
+                    {dog[0].temperaments.map(temperament => (
+                        <span className='temperament' key={temperament}>
+                            {temperament}
+                        </span>
+                    ))}
+                </div>
+            </div> : <div className="loader"></div>}
+        </div>
     );
 }
